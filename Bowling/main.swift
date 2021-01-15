5dd3a91a44f90721f2950a6412f1c24944fe6bf9
Salnikov Yegor <kontyolo34np@gmail.com>
2021-01-15 16:08:31 +0500

    1. Ввод данных, валидация, учитывание, вывод

--- Bowling/main.swift
+++ Bowling/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Добро пожаловать! Я - Ваш личный боулинг менеджер. Введите число сбитых вами кеглей")
 
+var isPlaying = false
+var result = 0
+
+func userInput() {
+    if let input = readLine() {
+        let inputInt = Int(input) ?? 0
+        if validation(resultToValidate: inputInt) {
+            result += inputInt
+        } else {
+            userInput()
+        }
+    }
+}
+
+func validation(resultToValidate: Int) -> Bool {
+    if resultToValidate > 10 {
+        print("Количество сбитых кеглей не может быть больше 10, введите корректное число")
+        return false
+    } else {
+        return true
+    }
+}
+
+func outputResult() {
+    print("Общее количество сбитых кеглей: \(result)")
+}
+
+func startGame() {
+    while !isPlaying {
+        userInput()
+        outputResult()
+    }
+}
+
+startGame()
