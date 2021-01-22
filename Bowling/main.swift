dbe36a89a1788f86fa537e0eec2bbc7d91560b72
Salnikov Yegor <kontyolo34np@gmail.com>
2021-01-22 15:25:47 +0500

    Реализована валидация при вводе отрицательных числе, и добавлен более красивый текст для результатов раунда

--- Bowling/main.swift
+++ Bowling/main.swift
@@ -10,52 +10,59 @@ print("Добро пожаловать! Я Ваш личный боулинг м
 let maxPinCount = 10
 var isSecondRoll = false
 
-var result = 0
+var result: UInt = 0
 var round = 0
 
 func startGame() {
     while round < 10 {
-        guard let input = readLine(), let inputInt = Int(input) else {
-            print("Введите только число без пробелов")
+        guard let input = readLine(), let downedPins = UInt(input) else {
+            print("Введите корректное число сбитых кеглей")
             continue
         }
-        if checkValidPinCount(inputInt) {
-            if isSecondRollNow(isSecondRoll, inputInt) {
-                isSecondRoll = true
-                
-                continue
-            } else {
-                isSecondRoll = false
-                
-            }
+        
+        let downedPinsInt = Int(downedPins)
+        if (0 ... maxPinCount).contains(downedPinsInt) {
+            handleRoll(downedPins)
         } else {
-            print("Количество сбитых кеглей не может быть больше 10")
+            print("Количество сбитых кеглей не может быть больше \(maxPinCount), повторите ввод")
         }
+    }
+}
+
+func handleRoll(_ downedPins: UInt) {
+    result += downedPins
+    isSecondRoll = !isSecondRoll && downedPins < maxPinCount
+    
+    if isSecondRoll {
+        print("Сделайте второй бросок")
+    } else {
+        round += 1
         outputResult()
     }
 }
 
-func checkValidPinCount(_ value: Int) -> Bool {
-    return (0...10).contains(value)
-}
-
 func outputResult() {
-    print("Общее количество сбитых кеглей: " + String(result))
-    print("Следующий ход! Ваш бросок: ")
-}
-
-func isSecondRollNow(_ isSecondaryRoll: Bool, _ countPin: Int) -> Bool {
-    if countPin < maxPinCount && isSecondaryRoll == false {
-        result += countPin
-        print("Второй бросок")
-        
-        return true
-    } else {
-        result += countPin
-        round += 1
-
-        return false
+    let downedPins = String(result)
+    
+    switch result {
+        case 0...10, 15...300:
+            switch downedPins.last {
+                case "1":
+                    print("Вы сбили " + downedPins + " кеглю")
+                case "2", "3", "4":
+                    print("Вы сбили " + downedPins + " кегли")
+                case "5", "6", "7", "8", "9", "0":
+                    print("Вы сбили " + downedPins + " кеглей")
+                default:
+                    break
+            }
+        case 11 ... 14:
+            print("Вы сбили " + downedPins + " кеглей")
+        default:
+            break
     }
+    
+    print("Следующий раунд! Ваш бросок...")
 }
 
 startGame()
