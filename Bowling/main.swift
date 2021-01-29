f2416b27eb23d9c0da8e78e5db9d205799953e2a
Salnikov Yegor <kontyolo34np@gmail.com>
2021-01-29 16:15:15 +0500

    Реализована поддержка спэров

--- Bowling/main.swift
+++ Bowling/main.swift
@@ -11,22 +11,25 @@ typealias WordForms = (one: String, three: String, many: String)
 
 print("Добро пожаловать! Я Ваш личный боулинг-менеджер. Введите число сбитых вами кегель.")
 
-let maxPinCount: UInt = 10
+let maxPinCount = 10
 var isSecondRoll = false
 
-var firstRollCount: UInt = 0
+var firstRollCount = 0
+var sumPinCount = 0
 
-var totalResult: UInt = 0
+var totalResult = 0
 var round = 0
+
+var history: [[Int]] = []
 
 func startGame() {
     while round < 10 {
-        guard let input = readLine(), let downedPins = UInt(input) else {
+        guard let input = readLine(), let downedPins = Int(input) else {
             print("Введите корректное число сбитых кегель")
             continue
         }
         
-        let supportedCounts: ClosedRange<UInt> = 0 ... 10
+        let supportedCounts: ClosedRange<Int> = 0 ... 10
         let remainingPins = maxPinCount - firstRollCount
         
         if supportedCounts.contains(downedPins) && downedPins <= remainingPins {
@@ -37,15 +40,27 @@ func startGame() {
     }
 }
 
-func handleRoll(_ downedPins: UInt) {
-    totalResult += downedPins
+func handleRoll(_ downedPins: Int) {
     isSecondRoll = !isSecondRoll && downedPins < maxPinCount
     
     if isSecondRoll {
         firstRollCount = downedPins
         print("Сделайте второй бросок")
     } else {
+        sumPinCount = firstRollCount + downedPins
+        
+        if history.last?.count == 2 && history.last?.reduce(0, +) == 10  {
+            sumPinCount = sumPinCount * 2
+        }
+        
+        if downedPins == maxPinCount {
+            history.append([downedPins])
+        } else {
+            history.append([firstRollCount, downedPins])
+        }
         firstRollCount = 0
+        
+        totalResult += sumPinCount
         round += 1
         
         outputResult()
@@ -59,15 +74,18 @@ func outputResult() {
         return
     }
     
-    print("Вы сбили " + count)
-    print("Следующий раунд! Ваш бросок...")
+    if round == 10 {
+        print("Игра закончена! Поздравляем, вы сбили " + count)
+    } else {
+        print("Следующий раунд! Ваш бросок...")
+    }
 }
 
 startGame()
 
 extension String {
     
-    init?(count: UInt, wordForms: WordForms) {
+    init?(count: Int, wordForms: WordForms) {
         var suitableForm = wordForms.many
         
         if count.getDigit(ofRank: 10) != 1 {
@@ -97,15 +115,15 @@ extension String {
     
 }
 
-extension UInt {
+extension Int {
     
-    func getDigit(ofRank rank: UInt) -> UInt? {
+    func getDigit(ofRank rank: Int) -> Int? {
         guard rank > 0 && rank <= self && (rank.isMultiple(of: 10) || rank == 1) else {
             return nil
         }
         
         let multiplier = rank * 10
-        return UInt(self / rank) - UInt(self / multiplier) * 10
+        return Int(self / rank) - Int(self / multiplier) * 10
     }
     
 }
